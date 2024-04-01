58e9a6d2955b04ee88fe136548eb75fdf3a1e538
vighnesantosh <santosh.vighne@advantive.com>
2024-04-01 09:57:31 +0530

    second chekin

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using Carter;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductRequest (string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+          
+          
+        }
+    }
+}
